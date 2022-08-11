619d3a72aaaa0e165b2ef5814d1a0aef8da4b979
Tennessine <aliefesobeli007@gmail.com>
2022-08-11 13:49:38 +0300

    Create grid

---
+++ app.js
@@ -0,0 +1,17 @@
+const etch = document.querySelector(".etch-container");
+
+function createGrid(container, size) {
+    size = (size > 100) ? 100 : size;
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++) {
+            let pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            row.appendChild(pixel);
+        }
+        container.appendChild(row);
+    }
+}
+
+createGrid(etch, 16);
